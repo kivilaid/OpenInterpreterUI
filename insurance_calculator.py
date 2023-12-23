29be488a3d4b3e1a81bc1108729caf75edbd0832
kivilaid <kivilaid@hotmail.com>
2023-12-23 20:30:30 +0200

    aider: Added sections "Trip", "Risk", "Options", "Insured" to the insurance calculator interface.
    # Aider chat conversation:
    
    USER: add sections "Trip", "Risk", "Options", "Insured"
    
    ASSISTANT: ```diff
    --- insurance_calculator.py
    +++ insurance_calculator.py
    @@ ... @@
     def main():
         st.title('Insurance Cost Calculator')
    
    +    with st.expander("Trip Details"):
    +        # Placeholder for trip details inputs
    +        st.date_input("Departure")
    +        st.date_input("Return")
    +        st.text_input("Destination")
    +
    +    with st.expander("Risk Factors"):
    +        # Placeholder for risk factors inputs
    +        st.selectbox("Travel activity level", ["Low", "Medium", "High"])
    +        st.selectbox("Accommodation type", ["Hotel", "Hostel", "Rental", "Other"])
    +
    +    with st.expander("Options"):
    +        # Placeholder for additional options
    +        st.checkbox("Include flight insurance")
    +        st.checkbox("Include luggage insurance")
    +
    +    with st.expander("Insured Individuals"):
    +        # Placeholder for insured individuals details
    +        st.number_input("Number of adults", min_value=1, max_value=10, value=1)
    +        st.number_input("Number of children", min_value=0, max_value=10, value=0)
    +
         age = st.number_input('Enter your age', min_value=18, max_value=100, value=30)
         bmi = st.number_input('Enter your BMI', min_value=10.0, max_value=50.0, value=22.5)
         smoking_status = st.selectbox('Do you smoke?', ('non-smoker', 'smoker'))
    ```
    
    This diff adds four new sections to the `insurance_calculator.py` file: "Trip Details", "Risk Factors", "Options", and "Insured Individuals". Each section is created using Streamlit's `expander` widget, which allows the user to expand or collapse the section to view or hide the inputs. The inputs themselves are placeholders for the actual data that would be collected for an insurance calculation.

--- insurance_calculator.py
+++ insurance_calculator.py
@@ -11,6 +11,27 @@ def calculate_insurance(age, bmi, smoking_status):
 def main():
     st.title('Insurance Cost Calculator')
 
+    with st.expander("Trip Details"):
+        # Placeholder for trip details inputs
+        st.date_input("Departure")
+        st.date_input("Return")
+        st.text_input("Destination")
+
+    with st.expander("Risk Factors"):
+        # Placeholder for risk factors inputs
+        st.selectbox("Travel activity level", ["Low", "Medium", "High"])
+        st.selectbox("Accommodation type", ["Hotel", "Hostel", "Rental", "Other"])
+
+    with st.expander("Options"):
+        # Placeholder for additional options
+        st.checkbox("Include flight insurance")
+        st.checkbox("Include luggage insurance")
+
+    with st.expander("Insured Individuals"):
+        # Placeholder for insured individuals details
+        st.number_input("Number of adults", min_value=1, max_value=10, value=1)
+        st.number_input("Number of children", min_value=0, max_value=10, value=0)
+
     age = st.number_input('Enter your age', min_value=18, max_value=100, value=30)
     bmi = st.number_input('Enter your BMI', min_value=10.0, max_value=50.0, value=22.5)
     smoking_status = st.selectbox('Do you smoke?', ('non-smoker', 'smoker'))
