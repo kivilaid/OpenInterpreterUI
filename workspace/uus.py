ac79aea4d7ba32f230f764238bc8894ab57a5041
kivilaid <kivilaid@hotmail.com>
2023-12-26 21:00:17 +0200

    aider: Updated file handling to support different file types and extract text from them.
    # Aider chat conversation:
    
    ASSISTANT: To implement this feature, we need to modify `workspace\uus.py`:
    
    1. Import the necessary packages for handling different file types (docx, pdf, xlsx, txt).
    2. Update the `create_upload_file()` function to handle different file types and extract text from them.
    3. Display the extracted text in a new plaintext textbox.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    workspace\uus.py
    ```python
    <<<<<<< SEARCH
    import streamlit as st
    =======
    import streamlit as st
    import textract
    import pandas as pd
    from docx import Document
    import PyPDF2
    >>>>>>> REPLACE
    ```
    
    workspace\uus.py
    ```python
    <<<<<<< SEARCH
        uploaded_file = st.file_uploader("Choose a file")
        if uploaded_file is not None:
            # To read file as bytes:
            bytes_data = uploaded_file.getvalue()
            st.write(bytes_data)
    =======
        uploaded_file = st.file_uploader("Choose a file")
        if uploaded_file is not None:
            file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
            st.write(file_details)
            if uploaded_file.type == "application/pdf":
                pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
                text = ""
                for page in range(pdf_reader.getNumPages()):
                    text += pdf_reader.getPage(page).extractText()
            elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
                doc = Document(uploaded_file)
                text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
            elif uploaded_file.type == "text/plain":
                text = uploaded_file.getvalue().decode()
            elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
                df = pd.read_excel(uploaded_file)
                text = df.to_string()
            else:
                st.error("File type not supported.")
                return
            st.text_area("Extracted Text", text, height=200)
    >>>>>>> REPLACE
    ```

--- workspace/uus.py
+++ workspace/uus.py
@@ -1,11 +1,31 @@
 import streamlit as st
+import textract
+import pandas as pd
+from docx import Document
+import PyPDF2
 
 def create_upload_file():
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
+        file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+        st.write(file_details)
+        if uploaded_file.type == "application/pdf":
+            pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
+            text = ""
+            for page in range(pdf_reader.getNumPages()):
+                text += pdf_reader.getPage(page).extractText()
+        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            doc = Document(uploaded_file)
+            text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
+        elif uploaded_file.type == "text/plain":
+            text = uploaded_file.getvalue().decode()
+        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            df = pd.read_excel(uploaded_file)
+            text = df.to_string()
+        else:
+            st.error("File type not supported.")
+            return
+        st.text_area("Extracted Text", text, height=200)
 
 if __name__ == "__main__":
     create_upload_file()
